06ea14912e1361d6661f0a92d3ce9136b658eac1
Jordan Molina <jordan.e.molina21@gmail.com>
2025-10-10 11:40:00 -0400

    implemented DTO

--- src/main/java/com/MuscleHead/MuscleHead/User.java
+++ src/main/java/com/MuscleHead/MuscleHead/User.java
@@ -1,78 +1,79 @@
 package com.MuscleHead.MuscleHead;
 
+import jakarta.persistence.Entity;
 
+@Entity
+public class User {
+    private Integer id;
+    private String name;
+    private String email;
+    private String username;
 
-public class User{
-String name;
-String email;
-String username;
-int id;
+    public User() {}
 
-public String getName() {
-    return name;
-}
-public void setName(String name) {
-    this.name = name;
-}
-public String getEmail() {
-    return email;
-}
-public void setEmail(String email) {
-    this.email = email;
-}
-public String getUsername() {
-    return username;
-}
-public void setUsername(String username) {
-    this.username = username;
-}
+    public User(Integer id, String name, String email, String username) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.username = username;
+    }
 
-public void setId(int id){
-    this.id = id;
-}
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
 
-public Integer getId(){
-    return id;
-}
-@Override
-public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((name == null) ? 0 : name.hashCode());
-    result = prime * result + ((email == null) ? 0 : email.hashCode());
-    result = prime * result + ((username == null) ? 0 : username.hashCode());
-    result = prime * result + id;
-    return result;
-}
-@Override
-public boolean equals(Object obj) {
-    if (this == obj)
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
         return true;
-    if (obj == null)
-        return false;
-    if (getClass() != obj.getClass())
-        return false;
-    User other = (User) obj;
-    if (name == null) {
-        if (other.name != null)
-            return false;
-    } else if (!name.equals(other.name))
-        return false;
-    if (email == null) {
-        if (other.email != null)
-            return false;
-    } else if (!email.equals(other.email))
-        return false;
-    if (username == null) {
-        if (other.username != null)
-            return false;
-    } else if (!username.equals(other.username))
-        return false;
-    if (id != other.id)
-        return false;
-    return true;
+    }
+
+
+    
 }
 
-
-
-}
