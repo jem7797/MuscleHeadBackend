74876b42da414f84c7a907851ffb3a17581be7d7
Jordan Molina <jordan.e.molina21@gmail.com>
2026-01-13 15:03:42 -0500

    Added automatic highest weight lifted updates

--- src/main/java/com/MuscleHead/MuscleHead/Routine/RoutineRepository.java
+++ src/main/java/com/MuscleHead/MuscleHead/Routine/RoutineRepository.java
@@ -7,8 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.MuscleHead.MuscleHead.User.User;
 
 public interface RoutineRepository extends JpaRepository<Routine, Long> {
-    
+
     List<Routine> findByUser(User user);
-    
+
     List<Routine> findByUserSub_id(String subId);
 }
